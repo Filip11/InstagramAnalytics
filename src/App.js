d099e34bc9e8e0004e39313f424d84eb835dcff4
Filip Drobnjakovic <filip.drobnjakovic@gmail.com>
2018-04-02 23:28:32 -0400

    additions to when to post and follower components with bootstrap

--- src/App.js
+++ src/App.js
@@ -5,25 +5,59 @@ import FollowerGrowth from './components/FollowerGrowth'
 import WhenToPost from './components/WhenToPost'
 //import node from './components/WhenToPostD3';
 import HeatMap from 'react-heatmap-grid';
+import ReactTooltip from 'react-tooltip'
+import SideNav from './components/sideMenu'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import whiteheart from "./images/heart-insta-pink.png"
+import pinkheart from "./images/heart-insta-pink.png"
+import clockWhite from "./images/clock-white.png"
+import clockPink from "./images/clock-pink.png"
+
+
 
 class App extends Component {
   render() {
     return (
+
+      /*
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-          <h1 className = "colorTest">Hello World</h1>
-          <h1 className = "colorTest1">Hello World</h1>
-          <h1 className = "colorTest2">Hello World</h1>
+        
         </header>
-        <FollowerGrowth /> 
+        <SideNav />
+        <FollowerGrowth />
         <WhenToPost />
       </div>
+      */
+      <Router>
+      <div className="App">
 
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        
+        
+        <ul>
+
+          <li>
+            <Link to="/FollowerGrowth"><img src={pinkheart} className="App-logo" alt="logo"/></Link>
+          </li>
+          <li>
+            <Link to="/WhenToPost"><img src={clockPink} className="App-logo" alt="logo"/></Link>
+          </li>
+        </ul>
+        <Route path="/FollowerGrowth" component={FollowerGrowth} />
+        <Route path="/WhenToPost" component={WhenToPost} />
+        </header>
+        <SideNav />
+      </div>
+
+    </Router>
       
     );
+    
   }
+
 }
 
 
--- src/components/FollowerGrowth.js
+++ src/components/FollowerGrowth.js
@@ -1,41 +1,40 @@
 import React, { Component } from 'react';
 import '../styles/FollowerGrowth.css';
 import { LineChart, Line, AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import Stats from './FollowerStats.js'
 
 const data = [
-      {name: 'Page A', uv: 4000},
-      {name: 'Page B', uv: 3000},
-      {name: 'Page C', uv: 2000},
-      {name: 'Page D', uv: 2780},
-      {name: 'Page E', uv: 1890},
-      {name: 'Page F', uv: 2390},
-      {name: 'Page G', uv: 3490},
+      {targetDate: 'Page A', Followers: 3000},
+      {targetDate: 'Page B', Followers: 3200},
+      {targetDate: 'Page C', Followers: 3300},
+      {targetDate: 'Page D', Followers: 3280},
+      {targetDate: 'Page E', Followers: 3390},
+      {targetDate: 'Page F', Followers: 3690},
+      {targetDate: 'Page G', Followers: 3790},
 ];
 const FollowerGrowth = () => (
-  <AreaChart width={730} height={250} data={data}
-    margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+  <div className="FollowerChart">
+  <AreaChart width={1030} height={500} data={data}
+    margin={{ top: 50, right: 30, left: 0, bottom: 0 }}>
     <defs>
     	
       <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-        <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-        <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+        <stop offset="20%" stopColor="#e1306c" stopOpacity={0.8}/>
+        <stop offset="80%" stopColor="#f77737" stopOpacity={0.8}/>
+        <stop offset="100%" stopColor="#fcaf45" stopOpacity={0.8}/>
       </linearGradient>
-      
 
-      <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-        <stop offset="5%" stopColor="#e1306c" stopOpacity={0.3}/>
-        <stop offset="95%" stopColor="#c13584" stopOpacity={0.8}/>
-      </linearGradient>
     </defs>
 
-   	<XAxis dataKey="name"/>
-   	<YAxis/>
-   	<CartesianGrid strokeDasharray="3 3"/>
+   	<XAxis dataKey="targetDate" stroke="white"/>
+   	<YAxis stroke="white"/>
+   	<CartesianGrid />
    	<Tooltip/>
    	<Legend />
-	<Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-    //<Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+	<Area type="monotone" dataKey="Followers" stroke="#833ab4" fillOpacity={1} fill="url(#colorUv)" />
   </AreaChart>
+  <Stats />
+  </div>
 )
 
 export default FollowerGrowth
